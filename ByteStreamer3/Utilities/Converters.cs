3ce9c9d2ae809823974904741e15aaac20175ed8
Rafael <Rafael@gmail.com>
2023-02-02 13:50:39 +0200

    Converters.cs added

---
+++ ByteStreamer3/Utilities/Converters.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/// <summary>
+/// Xaml convertes
+/// </summary>
+namespace ByteStreamer3.Utilities
+{
+    /// <summary>
+    /// invert boolean values
+    /// </summary>
+    public class BoolConverter : System.Windows.Data.IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool bVal = System.Convert.ToBoolean(value);
+            return (bVal == true) ? false : true;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool bVal = System.Convert.ToBoolean(value);
+            return (bVal == true) ? false : true;
+        }
+    }
+
+    public class EnumMatchToBooleanConverter : System.Windows.Data.IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+                              object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null)
+                return false;
+
+            string checkValue = value.ToString();
+            string targetValue = parameter.ToString();
+            return checkValue.Equals(targetValue,
+                     StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        public object ConvertBack(object value, Type targetType,
+                                  object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null)
+                return null;
+
+            bool useValue = (bool)value;
+            string targetValue = parameter.ToString();
+            if (useValue)
+                return Enum.Parse(targetType, targetValue);
+
+            return null;
+        }
+    }
+
+    public class RadioBoolToIntConverter : System.Windows.Data.IValueConverter
+    {
+        static int last;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int integer = (int)value;
+            if (integer == int.Parse(parameter.ToString()))
+            {
+                last = integer;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value == true)
+                return parameter;
+            else
+                return last;
+        }
+    }
+}
