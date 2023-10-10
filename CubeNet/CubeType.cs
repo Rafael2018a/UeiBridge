2bd38d89605168958d8133167ea5ceff9a10e1b7
Rafael <Rafael@gmail.com>
2023-10-10 15:40:55 +0300

    CubeType test added

--- CubeNet/CubeType.cs
+++ CubeNet/CubeType.cs
@@ -12,8 +12,13 @@ namespace UeiBridge.CubeNet
         public string Desc { get; set; }
         public List<string> PertainCubeList { get; set; }
 
+        public CubeType() { }
         public CubeType(string nickName, string desc, string signature)
         {
+            if ((null==nickName)||(null==desc)||(null==signature))
+            {
+                throw new ArgumentException("nickName or desc signature is null");
+            }
             NickName = nickName;
             Desc = desc;
             CubeSignature = signature;
@@ -21,11 +26,23 @@ namespace UeiBridge.CubeNet
         }
         public void AddCube( System.Net.IPAddress cubeIp)
         {
+            if (null==cubeIp)
+            {
+                throw new ArgumentException("null==cubeIp");
+            }
             PertainCubeList.Add(cubeIp.ToString());
         }
 
         public bool Equals(CubeType other)
         {
+            if (null==other)
+            {
+                return false;
+            }
+            if (this==other)
+            {
+                return true;
+            }
             bool a1 = this.CubeSignature == other.CubeSignature;
             bool a2 = this.NickName == other.NickName;
             bool a3 = this.TypeId == other.TypeId;
---
+++ UeiBridgeTest/CubeNetTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UeiBridge;
+using NUnit.Framework;
+using System.IO;
+using UeiBridge.Library;
+using UeiDaq;
+using System.Net;
+using CubeNet;
+using UeiBridge.CubeNet;
+
+
+namespace UeiBridgeTest
+{
+    [TestFixture]
+    class CubeNetTest
+    {
+        CubeType ct1;
+        CubeType ct1_same;
+        CubeType ct2;
+
+        [SetUp]
+        public void Setup()
+        {
+            ct1 = new CubeType("n", "d", "s");
+            ct1.AddCube(IPAddress.Parse("10.11.12.13"));
+            ct1_same = new CubeType("n", "d", "s");
+            ct1_same.AddCube(IPAddress.Parse("10.11.12.13"));
+
+            ct2 = new CubeType("n1", "d", "s");
+        }
+
+        [Test]
+        [TestCase("", "", null)]
+        [TestCase(null,"", null)]
+        public void CubeTypeTest(string nickName, string desc, string signature)
+        {
+            Assert.Throws<System.ArgumentException>( () => { CubeType ct = new CubeType(nickName, desc, signature); });
+        }
+        [Test]
+        public void CubeTypeCompareTest()
+        {
+            Assert.That(ct1.Equals(null), Is.EqualTo(false));
+            Assert.That(ct1.Equals(ct1), Is.EqualTo(true));
+            var b = ct1.Equals(ct1_same);
+            Assert.That(b, Is.EqualTo(true));
+            Assert.That(ct2.Equals(ct1), Is.EqualTo(false));
+        }
+        [Test]
+        public void AddCubeTest()
+        {
+            Assert.Throws<System.ArgumentException>( () => { ct1.AddCube(null); });
+
+            ct1.AddCube(IPAddress.Parse("1.2.3.4"));
+            ct1.AddCube(IPAddress.Parse("4.3.2.1"));
+            var p = ct1.PertainCubeList;
+            Assert.That(p.Count, Is.EqualTo(3));
+        }
+        [Test]
+        public void StringListCompareTest()
+        {
+            List<string> l1 = new List<string>(), l2 = new List<string>();
+            Assert.That(l1.SequenceEqual(l2), Is.EqualTo(true));
+        }
+
+    }
+}
